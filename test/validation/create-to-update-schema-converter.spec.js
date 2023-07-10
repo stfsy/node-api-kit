4b4de7c4b622bde48f15656d0998dbdc0051558d
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-07-10 18:16:14 +0200

    chore: add validators

---
+++ test/validation/create-to-update-schema-converter.spec.js
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+import { converter } from "../../lib/validation/create-to-update-schema-converter.js";
+
+describe('CreateToUpdateSchemaConverter', () => {
+
+    it('injects the immutability operator for fields marked as immutable', () => {
+        const parsedSchema = converter({
+            test: { type: 'string', immutable: true }
+        })
+        expect(parsedSchema.test.check).to.be.a('function')
+    })
+
+    it('removes the default value', () => {
+        const parsedSchema = converter({
+            test: { type: 'string', default: 'test' }
+        })
+        expect(parsedSchema.test.default).to.be.undefined
+    })
+    
+    it('marks objects as optional', () => {
+        const parsedSchema = converter({
+            test: { type: 'string', default: 'test' }
+        })
+        expect(parsedSchema.test.optional).to.be.true
+    })
+})
