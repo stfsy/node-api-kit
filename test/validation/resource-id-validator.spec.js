0b0d20cf440d5c70f92623efee6ecb725acc7305
Stefan Pfaffel <s.pfaffel@gmail.com>
2023-08-11 17:37:57 +0200

    feat: add resource id validator

---
+++ test/validation/resource-id-validator.spec.js
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+import { customAlphabet } from "nanoid";
+import { resourceIdAlphabet } from "../../lib/util/resource-id.js";
+import validator from '../../lib/validation/validator.js';
+import { useResourceIdValidator } from "../../lib/validation/validators/resource-id-validator.js";
+
+const nanoid = customAlphabet(resourceIdAlphabet)
+
+describe('ResourceIdValidator', () => {
+
+    it('provides a schema for resource ids', () => {
+        const schema = useResourceIdValidator()
+        expect(schema.$$root).to.equal(true)
+        expect(schema.pattern).not.to.be.undefined
+        expect(schema.pattern).not.to.be.null
+        expect(schema.empty).to.be.false
+        expect(schema.type).to.equal('custom')
+    })
+
+    it('validates resource ids successfully when compiled', async () => {
+        const resourceIdValidator = validator.compile(useResourceIdValidator())
+        const result = await resourceIdValidator(nanoid(21))
+        expect(result).to.be.true
+    })
+
+    it('does not validate invalid resource ids successfully when compiled', async () => {
+        const resourceIdValidator = validator.compile(useResourceIdValidator())
+        const result = await resourceIdValidator(nanoid(5))
+        expect(result).to.have.length(1)
+        expect(result.at(0).type).to.equal('stringPattern')
+    })
+})
