5266b7b19ac21e8f649367735d30c4d535cd7444
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-03-18 21:20:22 +0100

    fix(rate-limit): next call causes http error

---
+++ test/middlewares/rate-limit.spec.js
@@ -0,0 +1,144 @@
+import { expect } from "chai";
+import rateLimitMiddleware from "../../lib/middlewares/rate-limit.js";
+import requestMock from "./request-mock.js";
+import responseMock from "./response-mock.js";
+
+describe('RateLimitMiddleware', () => {
+    describe('if no ip was sent', () => {
+        const handler = rateLimitMiddleware({
+            isIpRateLimitReached: async () => false,
+            storeIpAccess: async () => true
+        })
+
+        it('returns status 429', (done) => {
+            const req = requestMock({
+                method: 'GET', headers: {}
+            })
+            const res = responseMock({
+                send: () => {
+                    try {
+                        expect(res._status()).to.equal(429)
+                        done()
+                    } catch (e) {
+                        done(e)
+                    }
+                }
+            })
+            handler(req, res, null)
+        })
+
+        it('returns status error object', (done) => {
+            const req = requestMock({
+                method: 'GET', headers: {}
+            })
+            const res = responseMock({
+                send: (payload) => {
+                    try {
+                        const object = JSON.parse(payload)
+                        expect(object.status).to.equal(429)
+                        expect(object.title).to.equal('Too Many Requests')
+                        done()
+                    } catch (e) {
+                        done(e)
+                    }
+                }
+            })
+            handler(req, res, null)
+        })
+    })
+
+    describe('if ip rate limit was not reached', () => {
+        const handler = rateLimitMiddleware({
+            isIpRateLimitReached: async () => false,
+            storeIpAccess: async () => true
+        })
+
+        it('calls next', (done) => {
+            const req = requestMock({
+                method: 'GET', headers: {
+                    'fastly-client-ip': '1'
+                }
+            })
+            handler(req, null, done)
+        })
+    })
+
+    describe('allows to configure ip request header', () => {
+        const handler = rateLimitMiddleware({
+            isIpRateLimitReached: async () => false,
+            storeIpAccess: async () => true,
+            ipRequestHeaderName: 'my-ip'
+        })
+
+        it('calls next', (done) => {
+            const req = requestMock({
+                method: 'GET', headers: {
+                    'my-ip': '1'
+                }
+            })
+            handler(req, null, done)
+        })
+    })
+
+    describe('if empty ip header name is allowed', () => {
+        const handler = rateLimitMiddleware({
+            isIpRateLimitReached: async () => false,
+            storeIpAccess: async () => true,
+            failIfNoIpPresent: false
+        })
+
+        it('calls next', (done) => {
+            const req = requestMock({
+                method: 'GET', headers: {}
+            })
+            handler(req, null, done)
+        })
+    })
+
+    describe('if ip rate limit was reached', () => {
+        const handler = rateLimitMiddleware({
+            isIpRateLimitReached: async () => true,
+            storeIpAccess: async () => true
+        })
+
+        it('returns status 429', (done) => {
+            const req = requestMock({
+                method: 'GET', headers: {
+                    'fastly-client-ip': '1'
+                }
+            })
+            const res = responseMock({
+                send: () => {
+                    try {
+                        expect(res._status()).to.equal(429)
+                        done()
+                    } catch (e) {
+                        done(e)
+                    }
+                }
+            })
+            handler(req, res, null)
+        })
+
+        it('returns status error object', (done) => {
+            const req = requestMock({
+                method: 'GET', headers: {
+                    'fastly-client-ip': '1'
+                }
+            })
+            const res = responseMock({
+                send: (payload) => {
+                    try {
+                        const object = JSON.parse(payload)
+                        expect(object.status).to.equal(429)
+                        expect(object.title).to.equal('Too Many Requests')
+                        done()
+                    } catch (e) {
+                        done(e)
+                    }
+                }
+            })
+            handler(req, res, null)
+        })
+    })
+})
