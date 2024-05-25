bb844d533d0c19fdb66c4b886d1835aaa32069a4
Stefan Pfaffel <s.pfaffel@gmail.com>
2024-05-25 19:41:55 +0200

    feat(resource-validator): throw before createSchema instance will be reused as updateSchema

--- test/validation/resource-validator.spec.js
+++ test/validation/resource-validator.spec.js
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { addValidationMessage, addValidationMessages } from "../../lib/validation/resource-validator.js";
+import { addValidationMessage, addValidationMessages, createValidatorFromSchema } from "../../lib/validation/resource-validator.js";
 import validator from "../../lib/validation/validator.js";
 
 describe('ResourceValidator', () => {
@@ -18,6 +18,28 @@ describe('ResourceValidator', () => {
             })
             expect(validator.messages['123']).to.equal('456')
             expect(validator.messages['abc']).to.equal('def')
+        })
+    })
+    describe('.createValidatorFromSchema', () => {
+        it('throws if createSchema leaked into updateSchema', () => {
+            const schema = {
+                $$type: "object",
+                address: {
+                    $$type: "object",
+                    street: {
+                        type: "string",
+                        empty: false
+                    }
+                }
+            }
+
+            expect(() => {
+                const validator = createValidatorFromSchema({
+                    createSchemaProvider: () => {
+                        return schema
+                    }
+                })
+            }).to.throw(/^Your `createSchema` leaked/)
         })
     })
 })
